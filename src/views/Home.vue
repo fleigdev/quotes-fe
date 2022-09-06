d3fb00f6f90603a0f356a8d246fd1cc15834960d
fdev <fleigdev@gmail.com>
2022-09-06 12:07:35 -0400

    first commitgi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="mx-auto max-w-screen-lg">
+      <div class="flex items-center flex-col justify-center">
+        <div class="btn" @click="$store.commit('GetRandomQuote')">
+          Get Random Quote
+        </div>
+        <p class="my-5 text-2x1 text-center text-gray-500">
+          <span> "{{ $store.state.quote.content }}" </span><br />
+          <span class="text-gray-400"> "{{ $store.state.quote.author }}" </span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
